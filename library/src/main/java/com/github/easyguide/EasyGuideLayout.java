a9807804c0484acc07d3a52d3c27a7686265810d
shenxiaoliang <shenxiaoliang@kaike.la>
2018-08-14 17:32:59 +0800

    first commit

---
+++ library/src/main/java/com/github/easyguide/EasyGuideLayout.java
@@ -0,0 +1,28 @@
+package com.github.easyguide;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * Created by shenxl on 2018/8/14.
+ */
+
+public class EasyGuideLayout extends FrameLayout {
+    public EasyGuideLayout(Context context) {
+        super(context);
+    }
+
+    public EasyGuideLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public EasyGuideLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public EasyGuideLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+}
